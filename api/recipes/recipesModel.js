1a12f19621b77a2281cf06a486c7dfea1118e15d
Chris Lau <chrislyc.lau@gmail.com>
2021-09-19 10:31:58 -0600

    finished insert

--- api/recipes/recipesModel.js
+++ api/recipes/recipesModel.js
@@ -31,12 +31,109 @@ const get = async(user_id) =>{
 
 };
 
-const getById = (recipe_id) =>{
+const getById = async(recipe_id) =>{
+    const recipe = await db("recipes")
+    .where("recipes.recipe_id",recipe_id)
+    .join("sources","sources.source_id","recipes.source_id")
+    .join("categories","categories.category_id","recipes.category_id")
+    .select(
+        "recipe_id",
+        "recipes.user_id",
+        "category_name as category",
+        "recipe_name",
+        "image_url",
+        "source_name as source"
+    ).first();
+    recipe.steps = await db("steps")
+        .where({recipe_id})
+        .orderBy("step_number","asc")
+        .select("instructions as description","step_id");
 
+        for(let step of recipe.steps){
+            step.ingredients = await db("ingredients_steps as i")
+            .join("ingredients","i.ingredient_id","ingredients.ingredient_id")
+            .leftJoin("units","units.unit_id","i.unit_id")
+            .where({step_id:step.step_id})
+            .select("ingredient_name","amount","unit_name as unit")
+        }
+    return recipe;
 };
 
-const insert = (data) =>{
+const insert = async(data) =>{
+    //check already existing
+    const categories = await db("categories")
+    .where({category_name:data.category_name});
+    //insert if exists
+    const category_id = categories.length === 0
+    ?
+    (await db("categories")
+        .insert({category_name:data.category_name})
+        .returning("category_id"))[0]
+    :
+    categories[0].category_id
+    //check already existing
+    const sources = await db("sources")
+    .where({source_name:data.source_name,user_id:data.user_id});
+    //insert if exists
+    const source_id = sources.length === 0
+    ?
+    (await db("sources")
+        .insert({source_name:data.source_name,user_id:data.user_id})
+        .returning("source_id"))[0]
+    :
+    sources[0].source_id
+    //recipe insert
+    const [recipe_id] = await db("recipes")
+    .insert({
+        recipe_name:data.recipe_name,
+        source_id,
+        user_id:data.user_id,
+        image_url:data.image_url,
+        category_id,
+    })
+    .returning("recipe_id");
+    //inserting steps
+    for(let j = 0; j < data.steps.length;j++){
+        const step = data.steps[j];
+        const ingredients = step.ingredients.map(({ingredient_name})=>({ingredient_name}));
 
+        const unit_ids = [];
+
+        for(let {unit_name} of step.ingredients){
+            const unit = await db("units")
+            .where({unit_name});
+
+            if(unit.length===0){
+                const [unit_id] = await db("units")
+                .insert({unit_name})
+                .returning("unit_id");
+                unit_ids.push(unit_id);
+            }
+        }   
+
+        const [step_id] = await db("steps")
+        .insert({
+            step_number:j+1,
+            instructions:step.instructions,
+            recipe_id
+        })
+        .returning("step_id");
+
+        const ingredient_ids = await db("ingredients")
+        .insert(ingredients)
+        .returning("ingredient_id");
+
+        const ingredients_steps = step.ingredients.map(({amount},i)=>({
+            amount,
+            step_id,
+            unit_id:unit_ids[i],
+            ingredient_id:ingredient_ids[i]
+        }));
+
+        await db("ingredients_steps")
+        .insert(ingredients_steps)
+    } 
+    return recipe_id;
 };
 
 const update = (data,recipe_id) =>{
--- api/recipes/recipesModel.test.js
+++ api/recipes/recipesModel.test.js
@@ -39,6 +39,130 @@ describe("recipesModel.js",()=>{
             expect(ingredient).toHaveProperty("ingredient_name");
             expect(ingredient).toHaveProperty("amount");
             expect(ingredient).toHaveProperty("unit");
+        });
+        test("returns empty array if user_id does not exist",async()=>{
+            expect(await model.get(100)).toHaveLength(0);
         })
+    });
+    describe("getById",()=>{
+        test("returns the correct shape",async()=>{
+            const recipe = await model.getById(1);
+            expect(recipe).toHaveProperty("recipe_id");
+            expect(recipe).toHaveProperty("user_id");
+            expect(recipe).toHaveProperty("source");
+            expect(recipe).toHaveProperty("category");
+            expect(recipe).toHaveProperty("recipe_name");
+            expect(recipe).toHaveProperty("image_url");
+            expect(recipe).toHaveProperty("steps");
+
+            expect(recipe.steps).toHaveLength(3);
+
+            const step = recipe.steps[0];
+
+            expect(step).toHaveProperty("description");
+            expect(step).toHaveProperty("ingredients");
+
+            expect(step.ingredients).toHaveLength(1);
+
+            const ingredient = step.ingredients[0];
+
+            expect(ingredient).toHaveProperty("ingredient_name");
+            expect(ingredient).toHaveProperty("amount");
+            expect(ingredient).toHaveProperty("unit");
+        });
+    });
+    describe("insert",()=>{
+        test("can insert into database",async()=>{
+            const recipe = {
+                user_id:1,
+                source_name:"grandmother",
+                category_name:"dinner",
+                recipe_name:"tacos",
+                image_url:"https://someimage.jpg",
+                steps:[
+                    {
+                        instructions:"cook them",
+                        ingredients:[
+                            {
+                                ingredient_name:"taco shell",
+                                amount:10,
+                                unit_name:"none"
+                            },
+                            {
+                                ingredient_name:"miced beef",
+                                amount:125,
+                                unit_name:"gram"
+                            }
+                        ]
+                    }
+                ]
+            }
+
+            const recipe_id = await model.insert(recipe);
+            expect(await db("recipes").where({recipe_id,recipe_name:"tacos"})).toHaveLength(1);
+            expect(
+                await db("recipes")
+                .where({recipe_id,recipe_name:"tacos",source_name:"grandmother",category_name:"dinner",image_url:"https://someimage.jpg"})
+                .join("sources","recipes.source_id","sources.source_id")
+                .join("categories","recipes.category_id","categories.category_id")
+            ).toHaveLength(1);
+            expect(
+                await db("steps")
+                .where({recipe_id})
+                .join("ingredients_steps as i","i.step_id","steps.step_id")
+                .join("ingredients","i.ingredient_id","ingredients.ingredient_id")
+                .leftJoin("units","units.unit_id","i.unit_id")
+            ).toHaveLength(2);
+        })
+    });
+    describe("update",()=>{
+        test("updated record in db",async()=>{
+            const recipe = {
+                user_id:1,
+                source:"grandmother",
+                category:"dinner",
+                recipe_name:"tacos",
+                image_url:"https://someimage.jpg",
+                steps:[
+                    {
+                        description:"cook them",
+                        ingredients:[
+                            {
+                                ingredient_name:"taco shell",
+                                amount:10,
+                            },
+                            {
+                                ingredient_name:"miced beef",
+                                amount:125,
+                                unit:"gram"
+                            }
+                        ]
+                    }
+                ]
+            }
+            const recipe_id = await model.update(recipe,1);
+            expect(await db("recipes").where({recipe_id,recipe_name:"tacos"})).toHaveLength(1);
+            expect(
+                await db("recipes")
+                .where({recipe_id,recipe_name:"tacos",source_name:"grandmother",category_name:"dinner",image_url:"https://someimage.jpg"})
+                .join("sources","recipes.source_id","sources.source_id")
+                .join("categories","recipes.category_id","categories.category_id")
+            ).toHaveLength(1);
+            expect(
+                await db("steps")
+                .where({recipe_id})
+                .join("ingredients_steps as i","i.step_id","steps.step_id")
+                .join("ingredients","i.ingredient_id","ingredients.ingredient_id")
+                .leftJoin("units","units.unit_id","i.unit_id")
+            ).toHaveLength(2);
+        });
+    });
+    describe("remove",()=>{
+        test("removed record from db",async()=>{
+            const [recipe_id] = await model.remove(1);
+            expect(
+                await db("recipes").where({recipe_id})
+            ).toHaveLength(0);
+        });
     });
 });
